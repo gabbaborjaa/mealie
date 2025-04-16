7474ed1dce7103a1a06323e841225a68161981c1
gabbaborjaa <gab.borja26@gmail.com>
2025-04-15 21:23:33 -0400

    Logout warning added

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -4,13 +4,23 @@ import { Button } from "react-bootstrap";
 
 function LogoutButton() {
     const handleLogout = async () => {
-        try {
-            await signOut(auth);
-            console.log("User logged out");
-            alert("User logged out");
-        } catch (error) {
-            console.error("Logout error:", error.message);
+        const confirmLogout = window.confirm("Are you sure you want to log out?");
+        if (confirmLogout) {
+            signOut(auth)
+                .then(() => {
+                    console.log("User logged out");
+                })
+                .catch((error) => {
+                    console.log("Error logging out:", error)
+                })
         }
+        // try {
+        //     await signOut(auth);
+        //     console.log("User logged out");
+        //     alert("User logged out");
+        // } catch (error) {
+        //     console.error("Logout error:", error.message);
+        // }
     };
 
     return <Button variant="outline-danger" onClick={handleLogout}>Logout</Button>;
