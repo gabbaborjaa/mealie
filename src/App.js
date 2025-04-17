4b056b843861a23b3b01183b0692c299c6073ca7
gabbaborjaa <gab.borja26@gmail.com>
2025-04-17 19:48:58 -0400

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import Mealie from "./Mealie";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App(){
-  return (
-    <div> 
-      <Mealie />
-    </div>
-  )
+function App() {
+    return (
+        <Mealie />
+    );
 }
 
 export default App;
