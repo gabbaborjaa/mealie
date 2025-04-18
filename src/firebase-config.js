e1b2aa6e9582840624edf9267735818e9c1e562a
gabbaborjaa <gab.borja26@gmail.com>
2025-04-18 12:47:58 -0400

    edits to dotenv

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,19 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
-
+// import dotenv from "dotenv";
+// dotenv.config()
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfQg7-uC0PoYCOiSO2lik-LWLyEGGWQ48",
-  authDomain: process.env.authDomain,
-  projectId: "mealie-e6c06",
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
+console.log("Firebase Config: ", firebaseConfig); // Debugging log
 // console.log("Firebase Config: ", firebaseConfig)
 const app = initializeApp(firebaseConfig); // Establish connection
-
+// console.log(process.env.authDomain)
 export const db = getFirestore(app);
 export const auth = getAuth(app);
